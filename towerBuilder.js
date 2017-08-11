07fe847d8b20d1a83986ebaf631f2834d581e3d2
Justin Baize <justin.baize@gJBc897ad.local>
2017-08-11 11:31:51 -0500

    towerBuilder.js completed

--- towerBuilder.js
+++ towerBuilder.js
@@ -12,45 +12,52 @@
 //   '*****'
 // ]
 
+/**
+  name: towerBuilder
+  description: (1). creates asterix
+               (2). create spaces
+               (3). Appends asterixes and spaces, pushes into output, then returns
+  param: nFloors (integer)
+  returns: ["  *  "," *** ","*****"]
+**/
 function towerBuilder(nFloors) {
-  let output = ["*"];
+  let output = [];
   const maxFloorLength = (nFloors*2)-1;
 
-  for (let i =1;i<nFloors;i++){
-    let newFloor = output[i-1]+"**";
-    let spaceDiff = 1+(maxFloorLength - newFloor.length)/2;
+  for (let i = 1;i<=nFloors;i++){
+    let astrixNeeded = returnAsterix(i);
+    let spacesToAdd = returnSpaces(maxFloorLength,astrixNeeded.length);
+    let newLine = spacesToAdd+astrixNeeded+spacesToAdd;
 
-    // const copy = spaceDiff;
-    // let spaces = returnSpaces(spaceDiff);
-    // let spaces2 = returnSpaces(copy);
-    //
-    // //repeat doesn't work because of returning invalid count value!!!
-    // newFloor = spaces+newFloor+spaces2;
-
-    output.push(newFloor);
+    output.push(newLine);
   }
 
-  function returnSpaces(integer){
-    return " ".repeat(integer);
+  /**
+    name: returnAsterix
+    description: returns stringified asterix as a function of interator (integer)
+    param: iterator (integer)
+    returns: "*"
+  **/
+
+  function returnAsterix(iterator){
+    let asterixStr = "**".repeat(iterator);
+    return asterixStr.slice(0,asterixStr.length-1);
   }
 
-  // for (let i=2;i<=nFloors;i++ ){
-  //   let newFloor = output[i-2]+"**";
-  //   console.log("maxFloorLength>>>",maxFloorLength);
-  //   console.log("newFloor.length>>>",newFloor.length);
-  //   let spacesNeededEachSide = (maxFloorLength - newFloor.length)/2;
-  //   console.log("spacesNeededEachSide>>>",spacesNeededEachSide);
-  //
-  //   if (i !== nFloors){
-  //     let spaces = " ".repeat(spacesNeededEachSide);
-  //
-  //     // newFloor = " ".repeat(spacesNeededEachSide)+newFloor+" ".repeat(copy);
-  //     newFloor = spaces+newFloor+spaces;
-  //   }
-  //   // console.log("output before",output);
-  //   output.push(newFloor)
-  //   // console.log("output after",output);
-  // }
+  /**
+    name: returnSpaces
+    description: Returns empty spaces depending on maxFloor length && astrixStr length
+    param1: maxFloor   (integer)
+    param2: astrLength (integer)
+    returns: empty spaces
+  **/
+
+  function returnSpaces(maxFloor,astLength){
+    //needed on each side
+    let spacesNeeded = (maxFloor-astLength)/2;
+    let spaces = " ".repeat(spacesNeeded);
+    return spaces;
+  }
 
   return output
 }
@@ -59,7 +66,7 @@ function towerBuilder(nFloors) {
 // towerBuilder(2) ==> [" * ","***"];
 // towerBuilder(3) ==> ["  *  "," *** ","*****"];
 
-console.log(towerBuilder(3))
-// console.log(towerBuilder(4))
+// console.log(towerBuilder(3))
+console.log(towerBuilder(4))
 // console.log(towerBuilder(5))
 // console.log(towerBuilder(6))
