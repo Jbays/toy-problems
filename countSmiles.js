ccf266027e5e5704500ce8cb930137fcbeafd719
Justin Baize <justin.baize@gJBc897ad.local>
2017-08-24 14:52:20 -0500

    Close to finish.  About 70 tests pass.

--- countSmiles.js
+++ countSmiles.js
@@ -14,14 +14,60 @@
 //
 // Example cases:
 //
-console.log(countSmileys([':)', ';(', ';}', ':-D']));       // should return 2;
-// countSmileys([';D', ':-(', ':-)', ';~)']);     // should return 3;
-// countSmileys([';]', ':[', ';*', ':$', ';-D']); // should return 1;
+// console.log(countSmileys([':)', ';(', ';}', ':-D']));       // should return 2;
+// console.log(countSmileys([';D', ':-(', ':-)', ';~)']));     // should return 3;
+// console.log(countSmileys([';]', ':[', ';*', ':$', ';-D'])); // should return 1;
+
+// these are all the tests not working
+// Expected: 2, instead got: 5
+// Testing for: ;~) , :> , :o) , ;~> , ;o> , :D , :>
+// Expected: 2, instead got: 3
+// Testing for: :D , ;) , :~) , ;-) , ;> , :( , :>
+// Expected: 3, instead got: 4
+// Testing for: :-> , :-) , :o) , :-> , :~D , :o> , :oD
+// Expected: 2, instead got: 4
+// Testing for: :~> , ;~) , :> , ;~( , ;) , :o( , :oD
+// Expected: 2, instead got: 3
+// Testing for: ;-) , :D , :( , ;oD , :D , :o> , :o>
+// Expected: 3, instead got: 4
+// Testing for: :D , :) , ;) , ;) , :~( , :( , ;~)
+// Expected: 3, instead got: 5
+// Testing for: ;> , ;) , :~> , ;) , ;oD , :> , :>
+// Expected: 2, instead got: 3
+// Testing for: ;~D , :) , ;D , :( , ;D , ;~> , :)
 
 
 //return the total number of smiling faces in the array
 function countSmileys(arr) {
-  console.log("arr>>>",arr);
+  let count = 0;
 
-  return "whatever";
+  arr.forEach((faceString)=>{
+    let eyes = false;
+    let nose = false;
+    let mouth = false;
+    let splitFace = faceString.split('');
+
+    splitFace.forEach((singleFaceSymbol)=>{
+      if ( singleFaceSymbol === ";" ||
+           singleFaceSymbol === ":" ){
+        eyes = true;
+      }
+
+      if ( singleFaceSymbol === "-" ||
+           singleFaceSymbol === "~" ){
+        nose = true;
+      }
+
+      if ( singleFaceSymbol === ")" ||
+           singleFaceSymbol === "D" ){
+        mouth = true;
+      }
+    })
+
+    if ( eyes && nose && mouth ) {
+      count++;
+    }
+  })
+
+  return count;
 }
