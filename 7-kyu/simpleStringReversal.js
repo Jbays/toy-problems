c7593fdf505699c71cb7024382c203114ec20564
jbays <jjbaize@gmail.com>
2018-02-07 14:01:09 -0600

    Pushing up a correct good-vs-evil.js

--- 7-kyu/simpleStringReversal.js
+++ 7-kyu/simpleStringReversal.js
@@ -1,3 +1,4 @@
+// https://www.codewars.com/kata/simple-string-reversal
 // In this Kata, we are going to reverse a string while maintaining spaces.
 //
 // For example:
@@ -10,21 +11,48 @@
 // solve("codewars") = "srawedoc"
 // More examples in the test cases. All input will be lower case letters and in some cases spaces.
 
+/**
+ * @name: solve
+ * @description: reverses a string.  Spaces stay at the same place.
+ * @param2: str
+ * @returns: array
+ **/
 function solve(str){
-  //calculates the index of all empty spaces
-  let spaceLoc = str.split('').map((item,index)=>{
-    if ( item === ' ' ){return index;}
-  }).filter((item)=>{ if ( item ){return item;}})
+  let output = "";
 
-  let spacelessReversed = str.replace(/\W/g, '').split('').reverse()
-
-  //this rebuilds the string
-  spaceLoc.forEach((item,index)=>{
-    spacelessReversed.splice(spaceLoc[index],0," ");
-  })
-
-  return spacelessReversed.join('')
+  for ( let i = str.length-1; i >= 0; i-- ) {
+    if ( str[i] !== " " ) {
+      output += str[i];
+    }
+    if ( str[output.length] === " " ) {
+      output += str[output.length];
+    }
+  }
+  return output;
 }
+
+//
+// /**
+//  * @name: solve
+//  * @description: reverses a string.  Spaces stay at the same place.
+//  * @param2: str
+//  * @returns: array
+//  **/
+// function solve(str){
+//   //calculates the index of all empty spaces
+//   let spaceLoc = str.split('').map((item,index)=>{
+//     if ( item === ' ' ){return index;}
+//   }).filter((item)=>{ if ( item ){return item;}})
+//
+//   let spacelessReversed = str.replace(/\W/g, '').split('').reverse()
+//
+//   //this rebuilds the string
+//   spaceLoc.forEach((item,index)=>{
+//     spacelessReversed.splice(spaceLoc[index],0," ");
+//   })
+//
+//   return spacelessReversed.join('')
+// }
 
 console.log("your code rocks >>>>",solve("your code rocks"))
 // console.log("expected answer>>>>>","skco redo cruoy")
--- 8-kyu/catYears-dogYears.js
+++ 8-kyu/catYears-dogYears.js
@@ -17,22 +17,44 @@
 // +9 dog years for second year
 // +5 dog years for each year after that
 
+//[human,cat,dog]
 function humanYearsCatYearsDogYears(years){
   let catYears = 15
   let dogYears = 15
 
   if ( years === 1 ) {
-    return [years,catYears,dogYears]
+    return [years, catYears, dogYears];
   }
 
   if ( years === 2 ) {
-    return [years, catYears+9,dogYears+9]
+    return [years,catYears+9,dogYears+9];
   }
 
-  return [years,(catYears+(years-2)*4)+9,(dogYears+(years-2)*5)+9]
+  //how to handle for years === 3??
+
+  if ( years >= 3 ) {
+    return [years,
+          ((years-2)*4+(catYears+9)),
+          ((years-2)*5+(dogYears+9))];
+  }
+
+
+
+
+
+
+  // if ( years === 1 ) {
+  //   return [years,catYears,dogYears]
+  // }
+  //
+  // if ( years === 2 ) {
+  //   return [years, catYears+9,dogYears+9]
+  // }
+  //
+  // return [years,(catYears+(years-2)*4)+9,(dogYears+(years-2)*5)+9]
 }
 
-console.log(humanYearsCatYearsDogYears(1));
-console.log(humanYearsCatYearsDogYears(2));
-console.log(humanYearsCatYearsDogYears(3));
-console.log(humanYearsCatYearsDogYears(4));
+console.log("after 1 year>>",humanYearsCatYearsDogYears(1));
+console.log("after 2 years>",humanYearsCatYearsDogYears(2));
+console.log("after 3 years>",humanYearsCatYearsDogYears(3));
+console.log("after 4 years>",humanYearsCatYearsDogYears(4));
