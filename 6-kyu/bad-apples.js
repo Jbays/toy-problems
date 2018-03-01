0e921f5e6735d5b5dedf5422929e5c19e7a0e67f
jbays <jjbaize@gmail.com>
2018-02-28 19:30:02 -0600

    Adding two simple solutions

---
+++ 6-kyu/bad-apples.js
@@ -0,0 +1,72 @@
+// https://www.codewars.com/kata/bad-apples/train/javascript
+
+// Help a fruit packer sort out the bad apples.
+//
+// There are 7 varieties of apples,
+//all packaged as pairs and stacked in a fruit box.
+//Some of the apples are spoiled.
+//The fruit packer will have to make sure the spoiled apples are either removed from the fruit box or replaced. Below is the breakdown:
+//
+// Apple varieties are represented with numbers, 1 to 7
+//
+// A fruit package is represented with a 2 element array [4,3]
+//
+// A fruit package with one bad apple, or a bad package,
+//is represented with [2,0] or [0,2]
+//
+// A fruit package with two bad apples, or a rotten package,
+//is represented with [0,0]
+//
+// A fruit box is represented with:
+//
+// [ [ 1, 3 ],
+//   [ 7, 6 ],
+//   [ 7, 2 ],
+//   [ 1, 3 ],
+//   [ 0, 2 ],
+//   [ 4, 5 ],
+//   [ 0, 3 ],
+//   [ 7, 6 ] ]
+// Write a program to clear the fruit box off bad apples.
+//
+// The INPUT will be a fruit box represented with a 2D array:
+//[[1,3],[7,6],[7,2],[1,3],[0,2],[4,5],[0,3],[7,6]]
+//
+// The OUTPUT should be the fruit box void of bad apples:
+//[[1,3],[7,6],[7,2],[1,3],[2,3],[4,5],[7,6]]
+//
+// Conditions to be met:
+//
+// 1.A bad package should have the bad apple replaced if there is another bad package with a good apple to spare. Else, the bad package should be discarded.
+//
+// 2.The order of the packages in the fruit box should be preserved. Repackaging happens from the top of the fruit box index = 0 to the bottom nth index. Also note how fruits in a package are ordered when repacking. Example shown in INPUT/OUPUT above.
+//
+// 3.Rotten packages should be discarded.
+//
+// 4.There can be packages with the same variety of apples, e.g [1,1], this is not a problem.
+
+function badApples(input) {
+  console.log("input>>>",input);
+  let output = [];
+  let spares = [];
+
+  let memo;
+  let donut = input.map((item,index)=>{
+    let findZerosIndex = item.indexOf(0);
+
+    if ( !memo && findZerosIndex > -1) {
+      memo = findZerosIndex === 0 ? item[1] : item[0]
+    } else {
+      return []
+      console.log("item>>>",item);
+      console.log("findZerosIndex>>>",findZerosIndex)
+      memo = undefined;
+    }
+    console.log("memo>>>",memo)
+
+    return item;
+  })
+}
+
+console.log("[[2,1],[6,4],[2,7]]",badApples([[0,2],[1,0],[6,4],[2,7]]))
+// console.log("[[7, 1], [7, 3], [2, 5], [2, 2], [5, 5], [4, 6], [2, 5], [2, 1]]",badApples([ [ 7, 1 ],[ 7, 3 ],[ 0, 2 ],[ 5, 0 ],[ 2, 2 ],[ 5, 5 ],[ 4, 6 ],[ 2, 5 ],[ 2, 1 ] ]))
