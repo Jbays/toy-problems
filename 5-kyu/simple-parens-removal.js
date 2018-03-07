dfa9e7664977e2d29ad759393bd38f6fdf93bb07
jbays <jjbaize@gmail.com>
2018-03-07 16:08:20 -0600

    Not quite working still.  Little problems here and there -- revealed by test cases

--- 5-kyu/simple-parens-removal.js
+++ 5-kyu/simple-parens-removal.js
@@ -8,57 +8,99 @@
 // solve("x-(-y-z)") = "x+y+z"
 // There are no spaces in the expression. Only two operators are given: "+" or "-".
 
+// function solve(s) {
+//   let arrayOfCharacters = s.split('')
+//   if ( arrayOfCharacters.indexOf('(') === -1 ) {
+//     return s;
+//   }
+//
+//   let removeParens = removeParentheses(s);
+//   let output = "";
+//
+//   function removeParentheses(string){
+//     if ( string[0] === "(" ) {
+//       return removeParentheses(string.slice(1,string.length-1));
+//     }
+//     return string;
+//   }
+//
+//   let distributedNegs = findNegatives(removeParens);
+//
+//   function findNegatives(string){
+//     let start = null;
+//     let sliced = null;
+//     for ( let i =0; i < string.length; i++ ) {
+//       if ( string[i] === "-" ) {
+//         start = string.indexOf("(");
+//         let end = string.indexOf(")")+1;
+//         sliced = string.slice(start,end)
+//         // console.log("sliced",sliced)
+//         break;
+//       }
+//     }
+//
+//     return string.slice(0,start-1)+sliced.replace(/-/g,"+");
+//   }
+//
+//   return distributedNegs.split('').filter((item)=>{
+//       if (item !== "(" && item !== ")"){
+//         return item;
+//       }
+//     }).join('');
+// }
+
 function solve(s) {
-  let arrayOfCharacters = s.split('')
-  if ( arrayOfCharacters.indexOf('(') === -1 ) {
-    return s;
-  }
+  let beginOfCenter = s.lastIndexOf('(')
+  let endOfCenter = s.indexOf(')')
+  let center = s.slice(beginOfCenter+1,endOfCenter);
+  // let remnant = s.slice(0,endOfCenter-1).split('').reverse().join('')
+  let remnant = s.slice(0,endOfCenter-2)
 
-  let removeParens = removeParentheses(s);
-  let output = "";
+  if ( s.length < 3 ) { return s }
 
-  function removeParentheses(string){
-    if ( string[0] === "(" ) {
-      return removeParentheses(string.slice(1,string.length-1));
+  function distributedNegatives(remainder,centerPoint){
+    console.log("remainder",remainder)
+    console.log("centerPoint",centerPoint)
+    if ( remainder.length === 1 ) {
+      return remainder+centerPoint;
     }
-    return string;
-  }
-
-  let distributedNegs = findNegatives(removeParens);
-
-  function findNegatives(string){
-    let start = null;
-    let sliced = null;
-    for ( let i =0; i < string.length; i++ ) {
-      if ( string[i] === "-" ) {
-        start = string.indexOf("(");
-        let end = string.indexOf(")")+1;
-        sliced = string.slice(start,end)
-        // console.log("sliced",sliced)
-        break;
-      }
+    if ( centerPoint.length === 1 && remainder[remainder.length-1] !== "+" ) {
+      centerPoint = "+"+centerPoint;
     }
 
-    return string.slice(0,start-1)+sliced.replace(/-/g,"+");
+    for ( let i = remainder.length-1; i >= 0; i-- ){
+      if ( remainder[i] !== "(" ){
+        if ( remainder[i] === "-" ){
+          centerPoint = centerPoint.split('').map((letter)=>{
+            if ( letter === "-" ){ return "+" }
+            else if ( letter === "+" ){ return "-" }
+            else { return letter }
+          }).join('')
+        }
+        if ( remainder[i] === "+" || remainder[i].match(/[a-zA-Z]/) ) {
+          centerPoint = remainder[i]+centerPoint;
+        }
+      }
+    }
+    return (centerPoint.length === 2 && centerPoint[0] === "+") ? centerPoint[1] : centerPoint
   }
 
-  return distributedNegs.split('').filter((item)=>{
-      if (item !== "(" && item !== ")"){
-        return item;
-      }
-    }).join('');
+  return distributedNegatives(remnant,center);
 }
 
 //working
 // console.log(solve("-x"),"-x");
+// console.log(solve("a-(b)"),"a-b");
+// console.log(solve("a-(-b)"),"a+b");
+// console.log(solve("x-(-y-z)"),"x+y+z");
+// console.log(solve("(((((((((-((-(((n))))))))))))))"),"n");
+// console.log(solve("a+(b)"),"a+b");
+
+
+//not working
 
 //untested
-// console.log(solve("a-(b)"),"a-b");
-console.log(solve("x-(-y-z)"),"x+y+z");
-// console.log(solve("(((((((((-((-(((n))))))))))))))"),"n");
-console.log(solve("a-(-b)"),"a+b");
-// (solve("a+(b)"),"a+b");
-// (solve("a+(-b)"),"a-b");
+console.log(solve("a+(-b)"),"a-b");
 // (solve("(((a-((((-(-(f)))))))))"),"a-f");
 // (solve("((((-(-(-(-(m-g))))))))"),"m-g");
 // (solve("(((((((m-(-(((((t)))))))))))))"),"m+t");
