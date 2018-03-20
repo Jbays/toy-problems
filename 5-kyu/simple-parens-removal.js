c679619476096c220d231cc93e957593ece1d9f1
jbays <jjbaize@gmail.com>
2018-03-20 18:21:52 -0500

    Making some progress on simple-parens-remover.  Sorta.

--- 5-kyu/simple-parens-removal.js
+++ 5-kyu/simple-parens-removal.js
@@ -8,99 +8,196 @@
 // solve("x-(-y-z)") = "x+y+z"
 // There are no spaces in the expression. Only two operators are given: "+" or "-".
 
-// function solve(s) {
-//   let arrayOfCharacters = s.split('')
-//   if ( arrayOfCharacters.indexOf('(') === -1 ) {
-//     return s;
-//   }
+//Marc's solution
+// function solve(input) {
+//   true => '+', false => '-'
+//   let str = '';
+//   let ops = [true];
+//   let lastop = true;
+//   let outop = true;
+//   let first = true;
+//   let char;
 //
-//   let removeParens = removeParentheses(s);
-//   let output = "";
+//   while (input.length > 0) {
+//     char = input.slice(0, 1);
+//     input = input.slice(1);
 //
-//   function removeParentheses(string){
-//     if ( string[0] === "(" ) {
-//       return removeParentheses(string.slice(1,string.length-1));
-//     }
-//     return string;
-//   }
+//     console.log("this is str>>>",str);
 //
-//   let distributedNegs = findNegatives(removeParens);
-//
-//   function findNegatives(string){
-//     let start = null;
-//     let sliced = null;
-//     for ( let i =0; i < string.length; i++ ) {
-//       if ( string[i] === "-" ) {
-//         start = string.indexOf("(");
-//         let end = string.indexOf(")")+1;
-//         sliced = string.slice(start,end)
-//         // console.log("sliced",sliced)
-//         break;
+//     if (char.match(/[a-z]/)) {
+//       const sign = ops.reduce((a, c) => {
+//         return a ? c : !c;
+//       }, true);
+//       console.log("sign",sign);
+//       if (first) {
+//         str += (sign ? '' : '-') + char;
+//         first = false;
+//       } else {
+//         if (!sign) {
+//           str += (outop ? '+' : '-') + char;
+//         }
+//         else {
+//           str += (outop ? '-' : '+') + char;
+//         }
 //       }
+//     } else if (char === '+') {
+//       outop = ops[ops.length - 1];
+//       lastop = true;
+//     } else if (char === '-') {
+//       outop = !ops[ops.length - 1];
+//       lastop = false;
+//     } else if (char === '(') {
+//       ops.push(lastop);
+//       outop = lastop;
+//       lastop = true;
+//     } else if (char === ')') {
+//       ops.pop();
 //     }
 //
-//     return string.slice(0,start-1)+sliced.replace(/-/g,"+");
+//
+//     // console.log(`Char: ${char}, Last: ${lastop}, Out: ${outop}, Ops: ${ops}, Str: ${str}`);
 //   }
 //
-//   return distributedNegs.split('').filter((item)=>{
-//       if (item !== "(" && item !== ")"){
-//         return item;
-//       }
-//     }).join('');
+//   return str;
 // }
 
-function solve(s) {
-  let beginOfCenter = s.lastIndexOf('(')
-  let endOfCenter = s.indexOf(')')
-  let center = s.slice(beginOfCenter+1,endOfCenter);
-  // let remnant = s.slice(0,endOfCenter-1).split('').reverse().join('')
-  let remnant = s.slice(0,endOfCenter-2)
+//Zubair's solution
+// const solve = (statement) => {
+//   let ghettoStack = "";
+//   let opp = {
+//     "-":"+",
+//     "+":"-",
+//     "(": " ",
+//     ")": " "
+//   }
+//   let newStatement = ""
+//   let oppositeMode = false;
+//
+//   for (letter of statement) {
+//     ghettoStack+=letter;
+//     let lastLetter = ghettoStack.length - 1;
+//     let secondLastLetter = ghettoStack.length - 2;
+//
+//     if ( (ghettoStack[secondLastLetter]+ghettoStack[lastLetter]) === "-(" ) {
+//       oppositeMode = !oppositeMode;
+//     } else if ( ghettoStack[lastLetter] === ")" ) {
+//       oppositeMode = false
+//     }
+//     if (oppositeMode) {
+//       let nextLetter = opp[letter] || letter;
+//       newStatement += nextLetter;
+//     } else {
+//       newStatement += letter;
+//     }
+//   }
+//   return newStatement
+// }
 
-  if ( s.length < 3 ) { return s }
+function solve(string){
+  console.log("original input>>",string);
+  let staging = "";
+  let mappedString = "";
 
-  function distributedNegatives(remainder,centerPoint){
-    console.log("remainder",remainder)
-    console.log("centerPoint",centerPoint)
-    if ( remainder.length === 1 ) {
-      return remainder+centerPoint;
+  let val = string.split('').reduce((a,b)=>{
+    if ( a.match(/[a-z]/) && b.match(/[a-z]/) ){
+      return a+b
+    } else {
+      return a
     }
-    if ( centerPoint.length === 1 && remainder[remainder.length-1] !== "+" ) {
-      centerPoint = "+"+centerPoint;
-    }
+  }).length
 
-    for ( let i = remainder.length-1; i >= 0; i-- ){
-      if ( remainder[i] !== "(" ){
-        if ( remainder[i] === "-" ){
-          centerPoint = centerPoint.split('').map((letter)=>{
-            if ( letter === "-" ){ return "+" }
-            else if ( letter === "+" ){ return "-" }
-            else { return letter }
-          }).join('')
-        }
-        if ( remainder[i] === "+" || remainder[i].match(/[a-zA-Z]/) ) {
-          centerPoint = remainder[i]+centerPoint;
-        }
-      }
-    }
-    return (centerPoint.length === 2 && centerPoint[0] === "+") ? centerPoint[1] : centerPoint
+  val = (string[0] === "-") ? 2*val : (2*val)-1;
+
+  console.log("this is val",val);
+
+  let snippet = string.indexOf("(");
+  let start = 0;
+
+  while ( staging.length < val ) {
+    // snippet = string.indexOf("(");
+
+
+    console.log("hello friends!",staging);
+    staging += "1"
   }
 
-  return distributedNegatives(remnant,center);
+  return staging;
+
+  //this adds the extra pluses
+  // for ( let i = 0; i < string.length; i++ ) {
+  //   if ( string[i].match(/[a-z]/) ){
+  //     if ( string[i-1] === "(" ){
+  //       mappedString += "+"+string[i];
+  //     } else {
+  //       mappedString += string[i];
+  //     }
+  //   } else {
+  //     mappedString += string[i];
+  //   }
+  // }
+  //
+  // console.log("mappedString",mappedString);
+  //
+  // for ( let i = 0; i < mappedString.length; i++ ) {
+  //   if ( mappedString[i].match(/[a-z]/) ){
+  //     staging += mappedString[i];
+  //   } else {
+  //
+  //   }
+  // }
+  //
+  // return staging;
+
+  //parse in chunks
+  // let firstParen = string.indexOf("(")
+  //
+  // firstParen = (firstParen > 0 ) ? firstParen : string.length;
+  //
+  // function parseInChunks(stringSlice){
+  //   console.log("this is stringSlice",stringSlice);
+  //   for ( let i = 0; i < stringSlice.length; i++ ){
+  //     // console.log("stringSlice[i]",stringSlice[i])
+  //
+  //     if ( stringSlice[i].match(/[a-z]/) ){
+  //       staging += stringSlice[i];
+  //     } else {
+  //       if ( stringSlice[i] === "+" ) {
+  //         staging+= stringSlice[i];
+  //       } else if ( stringSlice[i] === "(" ){
+  //
+  //       } else if ( stringSlice[i] === ")" ){
+  //
+  //       } else if ( stringSlice[i] === "-" ){
+  //         console.log("stringSlice[i]",stringSlice[i])
+  //       }
+  //     }
+  //   }
+  // }
+
+  // parseInChunks(string.slice(0,firstParen))
+
+  return staging;
 }
 
 //working
-// console.log(solve("-x"),"-x");
+// console.log("a+(-b):",solve("a+(-b)")==="a-b");
+
+
 // console.log(solve("a-(b)"),"a-b");
 // console.log(solve("a-(-b)"),"a+b");
 // console.log(solve("x-(-y-z)"),"x+y+z");
 // console.log(solve("(((((((((-((-(((n))))))))))))))"),"n");
 // console.log(solve("a+(b)"),"a+b");
-
+// console.log(solve("-(x-y)"),"-x+y");
+// console.log(solve("x-(y+z)"),"x-y-z");
+// console.log(solve("x-(y-z)"),"x-y+z");
 
 //not working
+// console.log(solve("-x"),"-x");
+console.log(solve("u-(v-w+(x+y))-z"),"u-v+w-x-y-z");
+// console.log(solve("x-(s-(y-z))-(a+b)"),"x-s+y-z-a-b");
 
 //untested
-console.log(solve("a+(-b)"),"a-b");
 // (solve("(((a-((((-(-(f)))))))))"),"a-f");
 // (solve("((((-(-(-(-(m-g))))))))"),"m-g");
 // (solve("(((((((m-(-(((((t)))))))))))))"),"m+t");
@@ -108,12 +205,7 @@ console.log(solve("a+(-b)"),"a-b");
 // (solve("-((-x))"),"x");
 // (solve("-(-(-x))"),"-x");
 // (solve("-(-(x-y))"),"x-y");
-// (solve("-(x-y)"),"-x+y");
-// (solve("x-(y+z)"),"x-y-z");
-// (solve("x-(y-z)"),"x-y+z");
 // (solve("x-(-((-((((-((-(-(-y)))))))))))"),"x-y");
-// (solve("u-(v-w+(x+y))-z"),"u-v+w-x-y-z");
-// (solve("x-(s-(y-z))-(a+b)"),"x-s+y-z-a-b");
 // (solve("u+(g+v)+(r+t)"),"u+g+v+r+t");
 // (solve("q+(s-(x-o))-(t-(w-a))"),"q+s-x+o-t+w-a");
 // (solve("u-(v-w-(x+y))-z"),"u-v+w+x+y-z");
