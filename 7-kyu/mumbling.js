3572190b5551b6ade48d622e2866049741325d43
jbays <jjbaize@gmail.com>
2018-05-08 17:53:51 -0700

    Think I got it working

---
+++ 7-kyu/mumbling.js
@@ -0,0 +1,22 @@
+// http://www.codewars.com/kata/mumbling/train/javascript
+
+function accum(s) {
+  let output = [];
+  s.split('').forEach((letter,index)=>{
+    let append = letter.toUpperCase();
+
+    if ( index > 0 ) {
+      append += letter.toLowerCase().repeat(index);
+    }
+
+    output.push(append);
+  })
+  return output.join('-');
+}
+
+
+// console.log(accum("ZpglnRxqenU") === "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu");
+// console.log(accum("NyffsGeyylB") === "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb");
+// console.log(accum("MjtkuBovqrU") === "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu");
+// console.log(accum("EvidjUnokmM") === "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm");
+// console.log(accum("HbideVbxncC") === "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc");
