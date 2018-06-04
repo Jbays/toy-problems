64d7dd68c5554f670caec3d863d464c0a35e5bca
jbays <jjbaize@gmail.com>
2018-06-04 11:02:02 -0500

    Readable, memorable newBranchName

--- 5-kyu/breakCamelCase.js
+++ 5-kyu/breakCamelCase.js
@@ -1,3 +1,5 @@
+//one actionable change
+
 // Complete the solution so that the function will break up camel casing,
 // using a space between words.
 
@@ -13,22 +15,12 @@ function solution(string){
   }
 
   //slice the original string into pieces using the indexes arr's indexes
-  indexes.forEach((slicePoint,index)=>{
-    //first case
-    if ( index === 0 ) {
-      output.push(string.slice(index,slicePoint));
-    } else {
-      //base case
-      output.push(string.slice(indexes[index-1],slicePoint))
-    }
 
-    //last case
-    if ( index === indexes.length-1 ) {
-      output.push(string.slice(slicePoint))
-    }
-  })
 
   return output.join(' ');
 }
 
 solution('camelCaseTesting') // => should return 'camel Casing'
+
+
+//this is the bottom of the file
