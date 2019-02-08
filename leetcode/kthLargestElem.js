cf06d9445e1cc7a86082a432768f4e9548aa7815
jbays <jjbaize@gmail.com>
2019-02-08 12:08:17 -0600

    Build a basic heap function.  Require further analysis.

--- leetcode/kthLargestElem.js
+++ leetcode/kthLargestElem.js
@@ -22,11 +22,89 @@
 
 //time complexity is O(n*log(n)) where n is the length of the input array (called nums)
 //Faster than 28% of solutions.  So between the bottom 1/4 and 1/2.
-function findKthLargest(nums, k) {
-  nums = nums.sort((a,b)=>{return a-b});
+// function findKthLargest(nums, k) {
+//   nums = nums.sort((a,b)=>{return a-b});
 
-  return nums[nums.length-k];
-};
+//   return nums[nums.length-k];
+// };
+
+function findKthLargest(array,k){
+  let heap = []
+
+  for ( let i = 0; i < k; i++ ) {
+    insert(array[i],heap);
+  };
+
+  console.log('this is heap now',heap);
+
+  for ( let i = k; i < array.length; i++ ) {
+    let min = heap[0];
+    if ( array[i] > min ) {
+      removeMin(heap,k);
+      insert(array[i],heap);
+    }
+  }
+
+  console.log('this is your heap',heap);
+  return heap[0];
+}
+
+function insert(val,array){
+  array.push(val);
+  upHeap(array,array.length-1);
+}
+
+function parent(index){
+  return Math.floor((index-1)/2);
+}
+
+function removeMin(array,k){
+  array[0] = array[array.length-1];
+  array.pop();
+  downHeap(array,0,k);
+}
+
+function downHeap(array,index,k){
+  let temp = 0;
+  let smallest = index;
+  let leftIndex = left(index);
+  let rightIndex = right(index);
+  if ( leftIndex < array.length && array[leftIndex] < array[smallest] ) {
+    smallest = leftIndex;
+  }
+  if ( rightIndex < array.length && array[rightIndex] < array[smallest] ) {
+    smallest = rightIndex;
+  }
+  if ( index !== smallest ) {
+    temp = array[index];
+    array[index] = array[smallest];
+    array[smallest] = temp;
+    downHeap(array,smallest);
+  }
+}
+
+function left(index){
+  return 2*index+1;
+}
+
+function right(index){
+  return 2*index+2;
+}
+
+function upHeap(array,index){
+  let temp = 0;
+  while ( index > 0 ) {
+    let parentIndex = parent(index);
+    if ( array[parentIndex] > array[index] ) {
+      temp = array[parentIndex];
+      array[parentIndex] = array[index];
+      array[index] = temp;
+      index = parentIndex;
+    } else {
+      break;
+    }
+  }
+}
 
 let input1 = [3,2,1,5,6,4]
 let k1 = 2;
