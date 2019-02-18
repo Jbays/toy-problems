66f76350efc5cf24bf5e0d1fef0ae448381d575a
jbays <jjbaize@gmail.com>
2019-02-18 16:38:01 -0600

    Practiced whiteboarding this solution.

--- leetcode/twoSum.js
+++ leetcode/twoSum.js
@@ -1,3 +1,19 @@
+/*
+
+  https://leetcode.com/problems/two-sum/
+
+  Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+  You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+  Example:
+
+  Given nums = [2, 7, 11, 15], target = 9,
+
+  Because nums[0] + nums[1] = 2 + 7 = 9,
+  return [0, 1].
+*/
+
 let nums = [2, 7, 11, 15];
 let arr2 = [3,2,4];
 let target = 9;
@@ -25,5 +41,16 @@ function twoSum(array,target){
     map.set(compliment,i);
   }
 }
+
+
+// function twoSum(array,target){
+//   let complimentHolder = {};
+  //for each element in the array
+    //calculate compliment = target - element
+    //check if complimentHolder contains compliment
+      //if so, return indices of loop and value from key-value pair
+    //enter compliment into complimentHolder
+//   return null;
+// }
 
 console.log(twoSum(nums,target))
