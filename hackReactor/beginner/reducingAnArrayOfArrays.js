bb3823c89f0f4984372613cbc8b45a47001e25da
jbays <jjbaize@gmail.com>
2019-02-25 15:04:06 -0600

    Got that last function working.  Plain and simple.

--- hackReactor/beginner/reducingAnArrayOfArrays.js
+++ hackReactor/beginner/reducingAnArrayOfArrays.js
@@ -4,12 +4,37 @@
     Given an array of arrays, return those arrays concatenated together.
 
   Examples
-  Input	Output
-  arrays:
-  [ [ 10, 20 ], [ 30 ], [ ] ]	[ 10, 20, 30 ]
-  arrays:
-  [ [ 22 ], [ 33, [ 44 ] ], [ 55 ] ]	[ 22, 33, [ 44 ], 55 ]
-  arrays:
-  [ ]	[ ]
-
+    Input	
+      arrays: [ [ 10, 20 ], [ 30 ], [ ] ]	
+    
+    Output
+      [ 10, 20, 30 ]
+    
+    Input
+      arrays: [ [ 22 ], [ 33, [ 44 ] ], [ 55 ] ]	
+    
+    Output
+      [ 22, 33, [ 44 ], 55 ]
+    
+    Input
+      arrays:[ ]
+    Output
+      []
 */
+
+function concatArrays(arrays) {
+  let output = [];
+
+  for ( let i = 0; i < arrays.length; i++ ) {
+    if ( Array.isArray(arrays[i]) ) {
+      output.push(...arrays[i]);
+    } else {
+      output.push(arrays[i]);
+    }
+  }
+  
+  return output;
+}
+
+console.log(concatArrays([[10, 20], [30], []]))
+console.log(concatArrays([[22], [33, [44]], [55]]	))
