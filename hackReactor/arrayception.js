4e7a8971f0c10fbd617914d70a3c5eba87580564
jbays <jjbaize@gmail.com>
2019-03-01 11:54:59 -0600

    Time complexity && space complexity are O(n)

--- hackReactor/arrayception.js
+++ hackReactor/arrayception.js
@@ -30,24 +30,38 @@ Arrayception
       0
 */
 
-function arrayception(array){
-  let count = 0;
+//time complexity  => O(n) where n is the length of the original array stringified
+//space complexity => O(n) also!
 
-  for ( let i = 0; i < array.length; i++ ) {
-    if ( Array.isArray(array[i]) ) {
-      recursiveCounter(array[i],count);
+function arrayception(array) {
+  const stringifiedArr = JSON.stringify(array);
+
+  let outputCount = 0;
+  let tempCount = 0;
+  for (let i = 0; i < stringifiedArr.length; i++) {
+    let currChar = stringifiedArr[i];
+
+    if (currChar === '[') {
+      tempCount++;
+    }
+    if (currChar === ']') {
+      tempCount--;
+    }
+    if (!isNaN(parseInt(stringifiedArr[i]))) {
+      if (outputCount < tempCount) {
+        outputCount = tempCount;
+      }
     }
   }
 
-  function recursiveCounter(array,tempCount){
-
-  }
-
-  return count;
+  return outputCount;
 }
 
-console.log(arrayception([[5], [[]]]	));
+// console.log(arrayception([[5], [[]]]	));
 // console.log(arrayception([10, 20, 30, 40]	));
 // console.log(arrayception([[10, 20], [[30, [40]]]]	));
 // console.log(arrayception([]));
 // console.log(arrayception([[[]]]	));
+// console.log(arrayception([[10, 20], [[30, [40]]]]	));
+// console.log(arrayception([1, [2], 3, [4, 5, [6], 7]]));
+// console.log(arrayception([[],[[],[],[0,[[]]],[]],[]]));
