39b37ff73c3969dbc6bf65e3fad3dfa3a5c4c009
jbays <jjbaize@gmail.com>
2019-05-21 12:32:00 -0500

    First commit for these two problems

---
+++ hackReactor/alphabetInventory.js
@@ -0,0 +1,16 @@
+/*
+
+  Alphabet Inventory
+    Given an alphabet and a string of text, 
+      write a method that tallies the count of each letter defined in said alphabet (case insensitive), 
+      then return the result of this tally.
+
+*/
+
+function alphaCount(alphabet,text){
+  
+}
+
+console.log(alphaCount('aBc', 'aabbccdd')) //=> "a:2,b:2,c:2"
+// console.log(alphaCount('x', 'Racer X is my friend! :)')) //=> "x:1"
+// console.log(alphaCount('123','o hai')) //=> "no matches"
